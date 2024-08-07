2dadaa9b96c963d2d437dcbdf5939fd7cba1765a
Mdfahimiftekhar <iftekharfahim3163@gmail.com>
2024-08-07 13:28:43 -0500

    MenueController and Menu/view update.

--- RestaurantProject/Controllers/MenuController.cs
+++ RestaurantProject/Controllers/MenuController.cs
@@ -1,15 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantProject.BLL;
+using RestaurantProject.Models;
 
-namespace RestaurantProject.Controllers {
-    public class MenuController : Controller {
-        /*
+namespace RestaurantProject.Controllers
+{
+	public class MenuController : Controller
+	{
+		/*
          * Read Appetizers
          * Read Entress
          * Read Deserts
          * Read Drinks
          */
-        public IActionResult Index() {
-            return View();
-        }
-    }
+
+		private readonly AppetizerService _appetizerService;
+		private readonly EntreeService _entreeService;
+		private readonly DesertService _desertService;
+		private readonly DrinkService _drinkService;
+
+		public MenuController(AppetizerService appetizerService, EntreeService entreeService, DesertService desertService, DrinkService drinkService)
+		{
+
+			_appetizerService = appetizerService;
+			_entreeService = entreeService;
+			_desertService = desertService;
+			_drinkService = drinkService;
+		}
+
+		public IActionResult Index()
+		{
+			var appetizers = _appetizerService.GetAppetizers();
+			var entrees = _entreeService.GetEntrees();
+			var deserts = _desertService.GetDeserts();
+			var drinks = _drinkService.GetDrinks();
+
+			var menuViewModel = new MenuViewModel
+			{
+				Appetizers = appetizers,
+				Entrees = entrees,
+				Deserts = deserts,
+				Drinks = drinks
+			};
+
+			return View(menuViewModel);
+
+		}
+
+
+	}
 }
